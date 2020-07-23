d2b462c7666edf3f7f5e40b46b1e2325b7f8844a
Jenaro Calvino <jen.calvineo@gmail.com>
2020-07-23 14:37:07 -0300

    Initial commit

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,14 +1,27 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
-// You can delete this file if you're not using it
+const axios = require("axios");
+const POST_NODE_TYPE = `CollectedNote`;
 
-/**
- * You can uncomment the following line to verify that
- * your plugin is being loaded in your site.
- *
- * See: https://www.gatsbyjs.org/docs/creating-a-local-plugin/#developing-a-local-plugin-that-is-outside-your-project
- */
-exports.onPreInit = () => console.log("Loaded gatsby-starter-plugin")
+exports.sourceNodes = async ({
+  actions,
+  createContentDigest,
+  createNodeId,
+}, { notesOwner }) => {
+  const { createNode } = actions;
+
+  const result = await axios.get(`https://collectednotes.com/${notesOwner}.json`);
+  if (result.status !== 200)
+    throw new Error("Could not fetch notes from collected notes API");
+
+  result.data.notes.forEach((note) =>
+    createNode({
+      ...note,
+      id: createNodeId(`${POST_NODE_TYPE}-${note.id}`),
+      internal: {
+        type: POST_NODE_TYPE,
+        contentDigest: createContentDigest(note),
+      },
+    })
+  );
+
+  return;
+};
